536d3666e7f3d83aa1d215a51b66d457e55621f6
TSturtledove <roberttortorelli@yahoo.com>
2016-12-05 14:58:13 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Single Page Application XHR 02 Season Sales
+
+Getting used to JSON
---
+++ js.js
@@ -0,0 +1,15 @@
+// var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+// var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+//
+//
+// var hohohoElement = document.getElementById("coloredReindeer").innerHTML;
+//
+// for(t=0; t<reindeer.length; t++) {
+//   hohohoElement=hohohoElement + colors[t] + " " + reindeer[t] + ", "
+//   console.log(hohohoElement)
+// }
+//
+// hohohoElement
+// console.log(hohohoElement)
+//
+// document.getElementById("coloredReindeer").innerHTML = hohohoElement;
